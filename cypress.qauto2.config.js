df0e8197312e94f23ffaf2a8b99e4c5aedf8247a
Konstantin Sadovets <sadovec.k@gmail.com>
2025-03-28 19:31:41 +0200

    Changed user for qauto2

--- cypress.qauto2.config.js
+++ cypress.qauto2.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   env: {
     baseUrl: "https://qauto2.forstudy.space/",
-    email: "example2142@example.com",
+    email: "example2143@example.com",
     password: "Test1test",
   },
 
